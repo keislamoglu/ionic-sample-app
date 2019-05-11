f54e7f80b977359184d70b4cb14d6ac1e6329621
keislamoglu <kadiremin@gmail.com>
2019-05-11 15:54:33 +0300

    feat: create global app config

---
+++ src/app/shared/app-config.ts
@@ -0,0 +1,5 @@
+export const AppConfig =  {
+    authToken: '9746efa4-bd5b-4d60-b16c-05623ec28733',
+    loginUrl: 'http://www.uzlastr.com/account/xhrlogin',
+    registerUrl: 'http://www.uzlastr.com/account/xhrregister'
+};
