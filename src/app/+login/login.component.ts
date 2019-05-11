e04930910f661ea02219b7e20a91a3cbfe508289
keislamoglu <kadiremin@gmail.com>
2019-05-11 15:53:21 +0300

    feat: implement login component

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -1,22 +1,42 @@
 import {Component, OnInit} from '@angular/core';
 import {NavController} from '@ionic/angular';
-import {AuthService} from '../shared/services';
+import {AlertService, AuthService} from '../shared/services';
 
 @Component({
     templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
+    email: string;
+    password: string;
+
     constructor(private _navController: NavController,
-                private _authService: AuthService) {
+                private _authService: AuthService,
+                private _alertService: AlertService) {
     }
 
     ngOnInit(): void {
         if (this._authService.isAuthenticated()) {
-            this._navController.navigateRoot('/case-files');
+            this.navToCaseFiles();
         }
+    }
+
+    signIn() {
+        this._authService.login(this.email, this.password).subscribe({
+            error: err => {
+                this._alertService.message({
+                    title: 'Başarısız Giriş',
+                    message: 'Girilen bilgiler hatalı',
+                });
+            },
+            next: user => this.navToCaseFiles()
+        });
     }
 
     navToRegister() {
         this._navController.navigateRoot('/register');
+    }
+
+    navToCaseFiles() {
+        this._navController.navigateRoot('/case-files');
     }
 }
