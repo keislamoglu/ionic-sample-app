d3ce9fd732a16dc5dc768e664e47430434e894c7
keislamoglu <kadiremin@gmail.com>
2019-05-11 15:52:56 +0300

    feat: implement auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,53 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ClientUser, BackendUser} from '../entity';
+import {map} from 'rxjs/operators';
+import {AppConfig} from '../app-config';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+    private _authenticated = false;
+
+    constructor(private _http: HttpClient) {
+    }
+
+    login(email: string, password: string) {
+        const headers = new HttpHeaders({'Auth-Token': AppConfig.authToken});
+        return this._http.post(AppConfig.loginUrl, {email, password}, {headers}).pipe(
+            map((resp: BackendUser) => {
+                const [name, middlename] = resp.name.split(' ');
+
+                const user: ClientUser = {
+                    id: resp.userId,
+                    name,
+                    middlename,
+                    lastname: resp.surname,
+                    email: resp.email,
+                    phone: resp.phone,
+                    sicilNumber: resp.sicilNumber,
+                    address: resp.address,
+                    commissioningDate: resp.commissioningDate,
+                    subscriptionStatus: resp.subscriptionStatus,
+                    subscriptionDate: resp.subscriptionDate
+                };
+                this._authenticated = true;
+                // TODO: save localStorage
+
+                return user;
+            })
+        );
+    }
+
+    logout() {
+        this._authenticated = false;
+        // TODO: remove from localStorage
+    }
+
     isAuthenticated() {
-        // TODO: implement authentication mechanism
-        return true;
+        return this._authenticated;
+    }
+
+    private _hashPassword(password: string) {
+        return password;
     }
 }
