c759268fb2f0f7095b7d9591385a94df06905d73
keislamoglu <kadiremin@gmail.com>
2019-05-11 15:52:25 +0300

    feat: separate user as client user and backend user

--- src/app/shared/entity/user.ts
+++ src/app/shared/entity/user.ts
@@ -1,10 +1,33 @@
-export class User {
+export class ClientUser {
     id: string;
     name: string;
     middlename: string;
     lastname: string;
-    nId: string;
-    address: string;
+    email: string;
     phone: string;
-    signature?: string;
+    address: string;
+    sicilNumber: string;
+    commissioningDate: string;
+    subscriptionStatus: SubscriptionStatus;
+    subscriptionDate: string;
+}
+
+export class BackendUser {
+    userId: string;
+    email: string;
+    name: string;
+    surname: string;
+    password: string;
+    sicilNumber: string;
+    phone: string;
+    address: string;
+    commissioningDate: string;
+    subscriptionStatus?: SubscriptionStatus;
+    subscriptionDate?: string;
+}
+
+export enum SubscriptionStatus {
+    Active = 1,
+    Trial = 2,
+    Expired = 3
 }
