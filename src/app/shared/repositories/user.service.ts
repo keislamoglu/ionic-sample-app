409f153bc5e2e1b99d7188a8d8726f5b51f28d4e
keislamoglu <kadiremin@gmail.com>
2019-05-11 15:54:04 +0300

    refactor: update user service implementation

--- src/app/shared/repositories/user.service.ts
+++ src/app/shared/repositories/user.service.ts
@@ -1,9 +1,67 @@
 import {Injectable} from '@angular/core';
-import {User} from '../entity';
-import {BaseCrud} from './base-crud';
+import {BackendUser, ClientUser, SubscriptionStatus} from '../entity';
 import {RepositoriesModule} from './repositories.module';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AppConfig} from '../app-config';
+import {map} from 'rxjs/operators';
+
+interface RegisterResp {
+    userId: string;
+    subscriptionStatus: SubscriptionStatus;
+    subscriptionDate: string;
+}
 
 @Injectable({providedIn: RepositoriesModule})
-export class UserService extends BaseCrud<User> {
-    protected dataSetName = 'users';
+export class UserService {
+
+    constructor(private _http: HttpClient) {
+    }
+
+    static toBackendUser(clientUser: ClientUser): BackendUser {
+        return {
+            userId: clientUser.id,
+            name: [clientUser.name, clientUser.middlename].filter(t => t).join(' '),
+            surname: clientUser.lastname,
+            password: void 0,
+            address: clientUser.address,
+            commissioningDate: clientUser.commissioningDate,
+            email: clientUser.email,
+            phone: clientUser.phone,
+            sicilNumber: clientUser.sicilNumber,
+            subscriptionDate: clientUser.subscriptionDate,
+            subscriptionStatus: clientUser.subscriptionStatus
+        };
+    }
+
+    static toClientUser(backendUser: BackendUser): ClientUser {
+        const [name, middlename] = backendUser.name.split(' ');
+
+        return {
+            id: backendUser.userId,
+            name,
+            middlename,
+            lastname: backendUser.surname,
+            email: backendUser.email,
+            phone: backendUser.phone,
+            sicilNumber: backendUser.sicilNumber,
+            address: backendUser.address,
+            commissioningDate: backendUser.commissioningDate,
+            subscriptionStatus: backendUser.subscriptionStatus,
+            subscriptionDate: backendUser.subscriptionDate
+        };
+    }
+
+    add(data: BackendUser): Observable<ClientUser> {
+        const headers = new HttpHeaders({'Auth-Token': AppConfig.authToken});
+        return this._http.post(AppConfig.registerUrl, data, {headers}).pipe(
+            map((resp: RegisterResp) => {
+                const user = UserService.toClientUser(data);
+                user.id = resp.userId;
+                user.subscriptionStatus = resp.subscriptionStatus;
+                user.subscriptionDate = resp.subscriptionDate;
+                return user;
+            })
+        );
+    }
 }
