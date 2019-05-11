c4a494ea942dec7f889c1b15f408ad87924d0e32
keislamoglu <kadiremin@gmail.com>
2019-05-11 15:53:34 +0300

    feat: implement register component

--- src/app/+register/register.component.ts
+++ src/app/+register/register.component.ts
@@ -1,13 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {NavController} from '@ionic/angular';
-import {AuthService} from '../shared/services';
+import {AlertService, AuthService} from '../shared/services';
+import {BackendUser} from '../shared/entity';
+import {UserService} from '../shared/repositories';
 
 @Component({
     templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnInit {
+    user: BackendUser = new BackendUser();
+
     constructor(private _navController: NavController,
-                private _authService: AuthService) {
+                private _userService: UserService,
+                private _authService: AuthService,
+                private _alertService: AlertService) {
     }
 
     ngOnInit(): void {
@@ -18,5 +24,16 @@ export class RegisterComponent implements OnInit {
 
     navToLogin() {
         this._navController.navigateRoot('/login');
+    }
+
+    register() {
+        this._userService.add(this.user).subscribe({
+            next: () => {
+                this.navToLogin();
+            },
+            error: () => {
+                this._alertService.message({title: 'Başarısız', message: 'Bir hata meydana geldi'});
+            }
+        });
     }
 }
