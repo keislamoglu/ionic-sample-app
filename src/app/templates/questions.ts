ba224bc5d74d998040a63ea7d9df0d7da5fad8a8
keislamoglu <kadiremin@gmail.com>
2019-06-08 15:33:01 +0300

    feat: add questions

--- src/app/templates/questions.ts
+++ src/app/templates/questions.ts
@@ -1,10 +1,12 @@
 import {TemplateDocument} from '../shared/entity';
 import {SegbisGorusmeTalepQuestions} from './segbis-gorusme-talep';
 import {KovusturmaUzlastirmaciGorusmeTutanagiQuestions} from './kovusturma-uzlastirmaci-gorusme-tutanagi';
+import {KovusturmaUzlasmaTelifQuestions} from './kovusturma-uzlasma-teklif';
 
 export const TemplateQuestions = {
     [TemplateDocument.SegbisGorusmeTalep]: SegbisGorusmeTalepQuestions,
     [TemplateDocument.Istinabe]: [],
     [TemplateDocument.UzlasmaGorusmesineDavet]: [],
-    [TemplateDocument.KovusturmaUzlastirmaciGorusmeTutanagi]: KovusturmaUzlastirmaciGorusmeTutanagiQuestions
+    [TemplateDocument.KovusturmaUzlastirmaciGorusmeTutanagi]: KovusturmaUzlastirmaciGorusmeTutanagiQuestions,
+    [TemplateDocument.KovusturmaUzlasmaTeklif]: KovusturmaUzlasmaTelifQuestions
 };
