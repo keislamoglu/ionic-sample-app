fca6e259167d198ce4a546f5ef0c53ff42fef241
keislamoglu <kadiremin@gmail.com>
2019-08-14 22:59:35 +0300

    refactor: ameliorate base template methods

--- src/app/templates/base/base-template.ts
+++ src/app/templates/base/base-template.ts
@@ -6,19 +6,27 @@ export abstract class BaseTemplate<T = {}> extends DocxFileTemplate {
     }
 
     protected printLabelValue(labelValueArray: Array<string[]>): void {
-        labelValueArray.forEach(([label, value]) => {
+        for (const [label, value] of labelValueArray) {
             const p = this.createP();
             this.addText(`${label}: `, p).bold();
-            this.addText(value, p);
-        });
+
+            if (value) {
+                this.addText(value, p);
+            }
+        }
     }
 
-    protected printIndentedLabelValue(labelValueArray: Array<string[]>): void {
-        labelValueArray.forEach(([label, value]) => {
+    protected printIndentedLabelValue(labelValueArray: Array<string[]>, offset = 1): void {
+        for (const [label, value] of labelValueArray) {
             const p = this.createP();
-            this.addText(`${label}: `, p).tab();
+            const text = this.addText(`${label}: `, p);
+
+            for (let i = 0; i < offset; i++) {
+                text.tab();
+            }
+
             this.addText(value, p);
-        });
+        }
     }
 
     protected printDate(dateString: string): string {
