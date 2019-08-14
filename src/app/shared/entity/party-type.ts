9738f2d4214fbaec464ef921f3bcfe2e08f61f53
keislamoglu <kadiremin@gmail.com>
2019-08-14 22:58:17 +0300

    feat: add new party types

--- src/app/shared/entity/party-type.ts
+++ src/app/shared/entity/party-type.ts
@@ -2,10 +2,15 @@ export enum PartyType {
     Injured = 'Mağdur', // Mağdur
     Suspected = 'Şüpheli', // Şüpheli
     Complainant = 'Müşteki', // Müşteki
-    InjuredsLegalRepresentative = 'Mağdurun kanuni temsilcisi', // Mağdurun kanuni temsilcisi
-    SuspectedsLegalRepresentative = 'Şüphelinin kanuni temsilcisi', // Sanığın / Şüphelinin kanuni temsilcisi
-    ComplainantsLegalRepresentative = 'Müştekinin kanuni temsilcisi', // Müştekinin kanuni temsilcisi
+    InjuredRepresentative = 'Mağdurun vekili', // Mağdurun vekili
+    SuspectedRepresentative = 'Şüphelinin vekili', // Şüphelinin vekili
+    InjuredAdvocate = 'Mağdurun müdafisi', // Mağdurun müdafisi, avukatı
+    SuspectedAdvocate = 'Şüphelinin müdafisi', // Sanığın / Şüphelinin müdafisi, avukatı
+    InjuredLegalDelegate = 'Mağdurun kanuni temsilcisi', // Mağdurun kanuni temsilcisi
+    SuspectedLegalDelegate = 'Şüphelinin kanuni temsilcisi', // Sanığın / Şüphelinin kanuni temsilcisi
+    ComplainantsLegalDelegate = 'Müştekinin kanuni temsilcisi', // Müştekinin kanuni temsilcisi
+    AffectedByCrimesLegalDelegate = 'Suçtan zarar görenin kanuni temsilcisi', // Suçtan zarar görenin kanuni temsilcisi
     AffectedByCrime = 'Suçtan zarar gören', // Suçtan zarar gören
-    AffectedByCrimesLegalRepresentative = 'Suçtan zarar görenin kanuni temsilcisi', // Suçtan zarar görenin kanuni temsilcisi
+    Translator = 'Tercüman',
     Other = 'Diğer'
 }
